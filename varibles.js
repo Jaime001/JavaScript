dda2891b864566250d8e9a8fd660e1344cbf617a
Jaime001 <jortiz10@ucol.mx>
2020-07-20 13:46:33 -0500

    Varibles en JS

---
+++ varibles.js
@@ -0,0 +1,28 @@
+/* Variable es un lugar en memoria
+Es una representación de algún lugar en la memoria para guardar datos
+ */
+
+//Declarar 
+var nombre;
+var edad;
+var correo;
+var contacto;
+var arreglo;
+
+//Inicializar
+nombre = "Jaime";
+edad = 21;
+correo = "jaime@gmail.com";
+contacto = ["telefono", "gmail"];
+arreglo = {
+  nombre,
+  contacto,
+  edad,
+  correo
+}
+
+//Llamar
+edad;
+correo;
+contacto;
+arreglo;
