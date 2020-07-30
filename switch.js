140db3afa2ec77f81d1952096dcf129253bf0ab7
Jaime001 <jortiz10@ucol.mx>
2020-07-30 12:02:15 -0500

    Uso de switch en JS

---
+++ switch.js
@@ -0,0 +1,15 @@
+var opcion = 1; 
+
+switch (opcion) {
+  case 1:
+    console.log("Optaste por la opcion 1!");
+    break;
+  case 2:
+    console.log("Optaste por la opcion 2!");
+    break;
+  case 20:
+    console.log("Optaste por la opcion 20!");
+    break;
+  default:
+    console.log("No seleccionaste ninguna opcion :(!");
+}
